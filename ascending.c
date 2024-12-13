77632946f717ad6de03d8a7b927408251cb718e2
Aashish-Chapagain <153161063+Aashish-Chapagain@users.noreply.github.com>
2024-12-13 09:53:30 +0545

    sort_descending.c

--- ascending.c
+++ ascending.c
@@ -1,15 +1,25 @@
 #include<stdio.h>
+#include<stdlib.h>
 int main()
 {
     int n; // to take input from the user for array size  
     printf("Enter the size of the array: ");
     scanf("%d",&n);
+    if (n<1){
+        printf("enter the valid number!");
+        return 1 ; 
+    }
     int *pSizeofarray = (int*)malloc(n * sizeof(int));
+    if (pSizeofarray == NULL){
+        printf("memory allocation failed "); 
+        return 1 ; 
+    }
     int arr[n],i,j; // to store the array elements
     for( i = 0; i < n; i++ )
     {
         printf("Enter number %d: ", i+1);
         scanf("%d",&arr[i]);
+        getchar();
     }
     printf("Sorted array: ");
     for(i = 0; i < n; i++ ){
@@ -24,6 +34,8 @@ int main()
         printf("%d ", arr[i]);
     }    
     printf("\n");
+
+    free(pSizeofarray);
 
     return 0;
 }
